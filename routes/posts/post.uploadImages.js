b85a6a760254ea6dc68ceaa184686ccdfcee32b2
chrisSmithCoding <heychrisgs@gmail.com>
2023-09-22 12:50:02 -0400

    connects to mongoDb

--- routes/posts/post.uploadImages.js
+++ routes/posts/post.uploadImages.js
@@ -1,12 +1,16 @@
+// Import the Mongoose library, which is used to interact with MongoDB.
 var mongoose = require('mongoose');
+
+// Define a schema for the "Image" model.
 var imageSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    img:
-    {
-        data: Buffer,
-        contentType: String
+    // Define fields for the schema:
+    name: String,   // Stores the name of the image.
+    desc: String,   // Stores a description of the image.
+    img: {          // Stores the image data and its content type.
+        data: Buffer,         // Binary image data stored as a Buffer.
+        contentType: String   // Content type of the image (e.g., image/jpeg).
     }
 });
- 
+
+// Export the schema as a Mongoose model named 'Image'.
 module.exports = mongoose.model('Image', imageSchema);
