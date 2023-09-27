3baeb8b77ed4ec7945f3054e41155f78afec385a
Richard <rdfioravanti111@gmail.com>
2023-09-27 12:22:37 -0400

    Increased timeout time to 10m

--- utilities/generateToken.js
+++ utilities/generateToken.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const generateAccessToken = (id, email, username, password ) => {
     return jwt.sign({id: id, email, username, password},process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:'1m'
+        expiresIn:'10m'
     })
  }
 
