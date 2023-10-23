8d703b1b5fc915f8c02832121b1b62ed737ac9db
chrisSmithCoding <heychrisgs@gmail.com>
2023-10-23 19:46:08 -0400

    backend router for remove images

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(require('./routes/users/user.editUser'))
 app.use(require('./routes/users/user.deleteall'))
 app.use(require('./routes/users/user.refresh-token'))
 app.use(require('./routes/statistics'))
-//app.use(require('./routes/posts/post.uploadImages'))
+
 
 // get driver connection
 const connectDB = require("./db/conn");
