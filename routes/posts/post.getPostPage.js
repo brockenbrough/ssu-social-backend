238fffae153a3a826ef7b84a48399599594eea25
karanssu <s0377009salemstate@gmail.com>
2024-10-01 15:02:21 -0400

    getPostPage code refactored

--- routes/posts/post.getPostPage.js
+++ routes/posts/post.getPostPage.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const Post = require("../../models/postModel");
 
 router.get("/posts/getPostPage", async (req, res) => {
-  const DEFAULT_POST_LIMIT = 10;
+  const DEFAULT_POST_PER_PAGE = 10;
   const INITIAL_PAGE = 1;
 
   const page = parseInt(req.query.page) || INITIAL_PAGE;
-  const limit = parseInt(req.query.limit) || DEFAULT_POST_LIMIT;
+  const postsPerPage = parseInt(req.query.limit) || DEFAULT_POST_PER_PAGE;
 
   try {
     const posts = await Post.find()
-      .skip((page - 1) * limit)
-      .limit(limit);
+      .skip((page - 1) * postsPerPage)
+      .limit(postsPerPage);
 
     return res.status(200).json(posts);
   } catch (error) {
