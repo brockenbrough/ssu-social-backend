8e14f3f26fc8537464c6d48742fe939d00e0a75c
karanssu <s0377009salemstate@gmail.com>
2024-10-01 15:05:02 -0400

    code refactored

--- routes/posts/post.getPostPage.js
+++ routes/posts/post.getPostPage.js
@@ -7,7 +7,7 @@ router.get("/posts/getPostPage", async (req, res) => {
   const INITIAL_PAGE = 1;
 
   const page = parseInt(req.query.page) || INITIAL_PAGE;
-  const postsPerPage = parseInt(req.query.limit) || DEFAULT_POST_PER_PAGE;
+  const postsPerPage = parseInt(req.query.postPerPage) || DEFAULT_POST_PER_PAGE;
 
   try {
     const posts = await Post.find()
