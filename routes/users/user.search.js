9d564a36eff7ee5e7f076e04ae2e96f80ee087cf
karanssu <s0377009salemstate@gmail.com>
2024-10-16 23:29:55 -0400

    user search uses fuzzysort

--- routes/users/user.search.js
+++ routes/users/user.search.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const newUserModel = require("../../models/userModel");
+const fuzzysort = require("fuzzysort");
 
 router.get("/user/search/:searchInput", async (req, res) => {
   const searchInput = req.params.searchInput;
@@ -10,11 +11,16 @@ router.get("/user/search/:searchInput", async (req, res) => {
   }
 
   try {
-    const users = await newUserModel.find({
-      username: { $regex: searchInput, $options: "i" },
+    const users = await newUserModel.find();
+
+    const results = fuzzysort.go(searchInput, users, {
+      key: "username",
+      threshold: -1000,
     });
 
-    return res.json(users);
+    const matchedUsers = results.map((result) => result.obj);
+
+    return res.json(matchedUsers);
   } catch (error) {
     console.error("Error searching for users:", error);
     return res.status(500).json({ error: "Internal server error" });
