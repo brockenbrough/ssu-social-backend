d64f597a243815409d1bae388af7fc6e55cdf702
Grummps <leitetravis@gmail.com>
2024-10-19 00:49:05 -0400

    Env variable changed

--- utilities/openaiService.js
+++ utilities/openaiService.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.CHAT_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
