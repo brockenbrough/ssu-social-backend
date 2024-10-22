dec35d2d2bc8eae8f87a0823b724cab011ed3389
karanssu <s0377009salemstate@gmail.com>
2024-10-22 14:13:31 -0400

    secured getMessageByChatRoomId route using tokens

--- routes/chat/message/getMessageByChatRoomId.js
+++ routes/chat/message/getMessageByChatRoomId.js
@@ -5,28 +5,32 @@ const verifyToken = require("../../../user-middleware/auth");
 const chatRoomModel = require("../../../models/chatRoomModel");
 const messageModel = require("../../../models/messageModel");
 
-router.get("/message/getByChatRoomId/:chatRoomId", async (req, res) => {
-  const { chatRoomId } = req.params;
+router.get(
+  "/message/getByChatRoomId/:chatRoomId",
+  verifyToken,
+  async (req, res) => {
+    const { chatRoomId } = req.params;
 
-  if (!chatRoomId) {
-    return res.status(400).json({ message: "ChatRoomId is required." });
-  }
-
-  try {
-    const roomExists = await chatRoomModel.exists({ _id: chatRoomId });
-    if (!roomExists) {
-      return res
-        .status(404)
-        .json({ message: `ChatRoomId with ID ${chatRoomId} not found.` });
+    if (!chatRoomId) {
+      return res.status(400).json({ message: "ChatRoomId is required." });
     }
 
-    const messages = await messageModel.find({ chatRoomId }).lean();
+    try {
+      const roomExists = await chatRoomModel.exists({ _id: chatRoomId });
+      if (!roomExists) {
+        return res
+          .status(404)
+          .json({ message: `ChatRoomId with ID ${chatRoomId} not found.` });
+      }
 
-    return res.json({ data: messages });
-  } catch (err) {
-    console.error("Error fetching messages:", err);
-    res.status(500).json({ error: "Could not fetch messages" });
+      const messages = await messageModel.find({ chatRoomId }).lean();
+
+      return res.json({ data: messages });
+    } catch (err) {
+      console.error("Error fetching messages:", err);
+      res.status(500).json({ error: "Could not fetch messages" });
+    }
   }
-});
+);
 
 module.exports = router;
