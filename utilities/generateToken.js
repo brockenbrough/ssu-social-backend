63ac52de0bb1f6035c2e47b564930ead63904542
Giovanni Cherisme <giocheris63@gmail.com>
2024-10-23 16:45:34 -0400

    Added role as a parameter

--- utilities/generateToken.js
+++ utilities/generateToken.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const generateAccessToken = (id, email, username ) => {
-    return jwt.sign({id: id, email, username},process.env.ACCESS_TOKEN_SECRET,{
+const generateAccessToken = (id, email, username,role ) => {
+    return jwt.sign({id: id, email, username, role},process.env.ACCESS_TOKEN_SECRET,{
         expiresIn:'60m'
     })
  }
