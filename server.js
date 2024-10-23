d98f2e7eda95692dc30481e3539c9b31d6a7e7f5
karanssu <s0377009salemstate@gmail.com>
2024-10-23 00:33:36 -0400

    chat refresh is not needed

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ const io = socketIo(server, {
 
 io.on("connection", (socket) => {
   socket.on("message", (data) => {
-    socket.broadcast.emit("message", data);
+    io.emit("message", data);
   });
 
   socket.on("disconnect", () => {});
