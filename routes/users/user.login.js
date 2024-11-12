41e099a9e3c3df7e44a0bd616374b6930be34657
Giovanni Cherisme <giocheris63@gmail.com>
2024-11-12 11:22:51 -0500

    Added roles to user token (fix)

--- routes/users/user.login.js
+++ routes/users/user.login.js
@@ -34,7 +34,7 @@ router.post('/user/login', async (req, res) => {
       .send({ message: "email or password does not exists, try again" });
 
   //create json web token if authenticated and send it back to client in header where it is stored in localStorage ( might not be best practice )
-  const accessToken = generateAccessToken(user.id, user.email, user.username,)
+  const accessToken = generateAccessToken(user.id, user.email, user.username, user.role)
 
   res.header('Authorization', accessToken).send({ accessToken: accessToken })
 })
